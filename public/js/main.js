baa6eb3718f78c946aa657a830debf0fc62e13ec
Kyle Plosky <31634240+kyleplo@users.noreply.github.com>
2026-09-02 21:35:11 -0400

    Implementation

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,55 @@
-// FRONT-END (CLIENT) JAVASCRIPT HERE
+window.addEventListener("load", async () => {
+  if (!localStorage.id) {
+    localStorage.id = Math.random().toString(36).slice(2);
+  }
 
-const submit = async function( event ) {
-  // stop form submission from trying to load
-  // a new .html page for displaying results...
-  // this was the original browser behavior and still
-  // remains to this day
-  event.preventDefault()
-  
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
-
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
+  document.querySelectorAll("input[name=id]").forEach(input => {
+    input.value = localStorage.id;
   })
 
-  const text = await response.text()
+  document.querySelectorAll("form").forEach(form => {
+    form.addEventListener("submit", async e => {
+      e.preventDefault();
 
-  console.log( 'text:', text )
-}
+      localStorage.vote = form.elements["vote"].value;
+      const data = await fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          id: form.elements["id"].value,
+          vote: form.elements["vote"].value
+        })
+      }).then(r => r.json());
+    displayData(data);
+    })
+  });
 
-window.onload = function() {
-  const button = document.querySelector('button')
-  button.onclick = submit
+  const data = await fetch("/api").then(r => r.json());
+  displayData(data);
+});
+
+function displayData(data) {
+  document.querySelector("#honeyCount").textContent = data.honey;
+  document.querySelector("#dirtCount").textContent = data.dirt;
+
+  document.querySelector("#honeyVoteList").innerHTML = "";
+  document.querySelector("#dirtVoteList").innerHTML = "";
+  for (const vote of data.votes) {
+    const listItem = document.createElement("LI");
+    listItem.textContent = `Someone voted for ${vote.vote} on ${new Date(vote.timestamp).toLocaleString()}`
+
+    if (vote.vote === "honey") {
+      document.querySelector("#honeyVoteList").append(listItem)
+    } else if (vote.vote === "dirt") {
+      document.querySelector("#dirtVoteList").append(listItem)
+    }
+  }
+
+  if (localStorage.vote && localStorage.vote !== "clear") {
+    document.body.className = "voted-" + localStorage.vote;
+  } else {
+    document.body.className = "";
+  }
 }
--- server.js
+++ server.js
@@ -1,74 +1,112 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you're testing this on your local machine.
-      // On Render, make sure `npm install` is your build command.
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require('http')
+const fs = require('fs')
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library if you're testing this on your local machine.
+  // On Render, make sure `npm install` is your build command.
+const mime = require('mime')
+const dir = 'public/'
+const port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+var votes = []
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === 'GET') {
+    handleGet(request, response)
+  } else if (request.method === 'POST') {
+    handlePost(request, response)
   }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1)
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === '/') {
+    sendFile(response, 'public/index.html')
+  } else if (request.url === '/api') {
+    response.writeHead(200, "OK", { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify(tallyVotes()))
+  } else {
+    sendFile(response, filename)
   }
 }
 
-const handlePost = function( request, response ) {
+const handlePost = function (request, response) {
   let dataString = ''
 
-  request.on( 'data', function( data ) {
-      dataString += data 
+  request.on('data', function (data) {
+    dataString += data
   })
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-    // ... do something with the data here!!!
+  request.on('end', function () {
+    try {
+      const data = JSON.parse(dataString);
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+      votes = votes.filter(entry => entry.id !== data.id);
 
-    // change this to incorporate data
-    response.end('test')
+      if (data.vote === "honey") {
+        votes.push({
+          id: data.id,
+          vote: "honey",
+          timestamp: Date.now()
+        })
+      } else if (data.vote === "dirt") {
+        votes.push({
+          id: data.id,
+          vote: "dirt",
+          timestamp: Date.now()
+        })
+      }
+
+      response.writeHead(200, "OK", { 'Content-Type': 'application/json' })
+      response.end(JSON.stringify(tallyVotes()))
+    } catch {
+      response.writeHead(400, "Server Error", { 'Content-Type': 'application/json' })
+      response.end(JSON.stringify({
+        error: "Invalid submission"
+      }))
+    }
   })
 }
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename)
 
-   fs.readFile( filename, function( err, content ) {
+  fs.readFile(filename, function (err, content) {
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { 'Content-Type': type })
+      response.end(content)
 
-     }else{
+    } else {
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+      // file not found, error code 404
+      response.writeHeader(404)
+      response.end('404 Error: File Not Found')
 
-     }
-   })
+    }
+  })
 }
 
-server.listen( process.env.PORT || port )
+function tallyVotes () {
+  var honey = 0;
+  var dirt = 0;
+  
+  for (const vote of votes) {
+    if (vote.vote === "honey") {
+      honey++;
+    } else if (vote.vote === "dirt") {
+      dirt++;
+    }
+  }
+
+  return {
+    honey,
+    dirt,
+    votes: votes.map(vote => ({vote: vote.vote, timestamp: vote.timestamp}))
+  }
+}
+
+server.listen(process.env.PORT || port)
